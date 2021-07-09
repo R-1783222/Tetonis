778e0f2b91a75b619c16fe3a655f71adcf74d0c4
R1783222 <c20015@std.it-college.ac.jp>
2021-07-09 16:39:32 +0900

    キャラがミノに当たったときの判定がより正確になるように調整した

---
+++ Assets/Scrips/GameManagement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagement : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+    public int currentScore;
+
+    public Text timerText;
+
+    public float gameTime = 60f;
+    int seconds;
+
+    public GameObject gamePauseUI;
+
+
+    void Start()
+    {
+        Initialize();
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeManagement();
+    }
+
+    private void Initialize()
+    {
+        score = 0;
+    }
+
+    public void TimeManagement()
+    {
+        gameTime -= Time.deltaTime;
+        seconds = (int)gameTime;
+        timerText.text = seconds.ToString();
+
+        if (seconds == 0)
+        {
+            GameOver();
+        }
+    }
+    public void AddScore()
+    {
+        score += 100;
+        scoreText.text = "Score:" + score.ToString(); 
+    }
+
+    public void GameOver()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActive().name);
+    }
+
+    public void GamePause()
+    {
+        GamePauseToggle();
+    }
+
+    public void GamePauseToggle()
+    {
+        gamePauseUI.SetActive(!gamePauseUI.activeSelf);
+
+        if (gamePauseUI.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
---
+++ Assets/Scrips/PlayerMove.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    float x= 1;
+    public int y = 1;
+    float speed;
+    Rigidbody2D rigidbody2D;
+    public Vector3 rotationPoint;
+
+    public enum MOVE_DIRECTION
+    {
+        STOP,
+        LEFT,
+        RIGHT,
+    }
+    MOVE_DIRECTION moveDirection = MOVE_DIRECTION.STOP; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (x == 0)
+        {
+            moveDirection = MOVE_DIRECTION.STOP;
+        }
+        else if (x > 0)
+        {
+            moveDirection = MOVE_DIRECTION.RIGHT;
+        }
+        else if ( x < 0)
+        {
+            moveDirection = MOVE_DIRECTION.LEFT;
+        }
+    }
+    private void FixedUpdate()
+    {
+        switch (moveDirection)
+        {
+            case MOVE_DIRECTION.STOP:
+                speed = 0 * y;
+                break;
+
+            case MOVE_DIRECTION.LEFT:
+                speed = -1 * y;
+                break;
+
+            case MOVE_DIRECTION.RIGHT:
+                speed = 1 * y;
+                break;
+        }
+        rigidbody2D.velocity = new Vector2(speed,rigidbody2D.velocity.y);
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (x > 0)
+        {
+            moveRight();
+        }
+        else if (x < 0)
+        {
+            moveLeft();
+        }
+
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    public void moveRight()
+    {
+        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 1, 0), 180);
+        x = -1;
+        Debug.Log("当たった");
+    }
+
+    public void moveLeft()
+    {
+        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 1, 0), -180);
+        x = 1;
+        Debug.Log("当たった");
+    }
+}
